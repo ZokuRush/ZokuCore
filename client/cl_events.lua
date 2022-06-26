0c87fa8f45bb51479c86433ea260b6a0acff1f36
ZokuRush <67478346+ZokuRush@users.noreply.github.com>
2022-06-26 00:28:44 -0600

    Update cl_events.lua

--- client/cl_events.lua
+++ client/cl_events.lua
@@ -416,3 +416,36 @@ AddEventHandler('ZokuCore:oxygentank', function()
 		exports['mythic_notify']:DoHudText('error', 'You are already wearing an oxygen tank!')
 	end
 end)
+
+-- No Seat Shuff
+local disableShuffle = true
+
+Citizen.CreateThread(function()
+	while true do
+		local ped = GetPlayerPed(-1)
+		local veh = GetVehiclePedIsIn(ped)
+
+		if IsPedInAnyVehicle(ped, false) and disableShuffle then
+			if GetPedInVehicleSeat(veh, false, 0) == ped then
+				if GetIsTaskActive(ped, 165) then
+					SetPedIntoVehicle(ped, veh, 0)
+				end
+			end
+		end
+
+		Citizen.Wait(2)
+	end
+end)
+
+RegisterNetEvent("ZokuCore:client:Shuff")
+AddEventHandler("ZokuCore:client:Shuff", function()
+	local ped = GetPlayerPed(-1)
+	if IsPedInAnyVehicle(ped, false) then
+		disableShuffle = false
+		SetTimeout(5000, function()
+			disableShuffle = true
+		end)
+	else
+		CancelEvent()
+	end
+end)
